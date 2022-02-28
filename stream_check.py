20f49cde781345243be79e1907724cb58f2078e5
Josh <joshfokis@gmail.com>
2022-02-27 18:45:11 -0600

    added docstrings

--- stream_check.py
+++ stream_check.py
@@ -1,9 +1,17 @@
-import json
 import os
 import requests
 from tinydb import TinyDB, Query
-from dotenv import load_dotenv
+import logging
 
+logging.basicConfig(
+    filename='Stream_check.log',
+    format='%(asctime)s %(message)s',
+    filemode='w'
+)
+
+logger = logging.getLogger()
+
+logger.setLevel(logging.DEBUG)
 # Init Database document
 db = TinyDB('streaming.json')
 
@@ -17,32 +25,54 @@ class Arr:
         self.remove = remove
         self.url = f"http://{self.host}/api/v3/"
 
-    # get media from *arr service
     def get_media(self):
+        """Get media from *arr service
+
+        Returns:
+            dict: returns dictionary from url request
+        """
         return get(url=f'{self.url}{self.media_type}?apikey={self.apikey}')
 
-    # set monitor status for media in *arr
     def start_monitor(self, ids):
+        """Sets the monitor status to True
+
+        Args:
+            ids (list): Receives a list of media IDs from the *arr service
+
+        Returns:
+            tuple: Currently returns the tuple from the requests module
+        """
         data = {
           "movieIds": ids,
-          "monitored": true,
+          "monitored": 'true',
         }
         return put(url=f'{self.url}movie/editor?apikey={self.apikey}', data=data)
 
-    # set monitor status for media in *arr
-    def stop_monitor(self, data):
+    def stop_monitor(self, ids):
+        """Sets the monitor status to False
+
+        Args:
+            data (list): Receives a list of media IDs from the *arr service
+
+        Returns:
+            tuple: Currently returns the tuple from the requests module
+        """
         data = {
           "movieIds": ids,
-          "monitored": false,
+          "monitored": 'false',
         }
         return put(url=f'{self.url}movie/editor?apikey={self.apikey}', data=data)
 
-    # remove media files if remove_media=True
-    def remove_media(self):
+    def remove_media(self, ids):
+        """Removes media files and folder when config remove is True
+
+        Args:
+            ids (list): Receives a list of media IDs from the *arr service
+        """
         if self.remove:
-            pass
-        pass
-
+            for i in ids:
+               delete(url=f'{self.url}/moviefile/{i}?apikey={self.apikey}') 
+        return
 
 class TMDB:
 
@@ -50,14 +80,28 @@ class TMDB:
         self.apikey = apikey
         self.country = country
 
-    # get all rent, buy, streaming providers for the media
     def get_services(self, media_type, media_id):
+        """Gets rent,buy, and flatrate streaming providers for the media
+
+        Args:
+            media_type (str): Receives a string of movie or series
+            media_id (int): Receives an int of the media id for The Movie Database
+
+        Returns:
+            dict: Returns dictionary of the streaming options
+        """
         url = f"https://api.themoviedb.org/3/{media_type}/{media_id}/watch/providers?api_key={self.apikey}"
         return self._parse_country(get(url=url))
 
-    # Parse out all the selected country
     def _parse_country(self, services):
+        """Parses the services of the users country
 
+        Args:
+            services (dict): Receives a dict of the services for a media
+
+        Returns:
+            dict: Returns a dict of the users country streaming options
+        """
         if "results" in services:
            if self.country in services.get('results').keys(): 
                #return contry results if the country is found
@@ -81,9 +125,13 @@ class Config:
         self.user_services = self._get_user_services()
         self.country = self._get_country()
 
-    # get the config from .env file
     # TODO: look at changing to .conf
     def _get_config(self):
+        """Opens and reads the config file
+
+        Returns:
+            dict: Returns a dict of all the key = value pairs
+        """
         config = {}
 
         # read .env in and create a dict based on key = value
@@ -93,24 +141,55 @@ class Config:
                 config[a[0].strip()] = a[1].strip()
         return config
 
-    # get user subscriptions from config as a list
     def _get_user_services(self):
+        """Get user's streaming subscriptions in config dict
+
+        Returns:
+            list: Returns a list of the users streaming subscriptions
+        """
         return [s.strip() for s in self.config.get('MY_SERVICES').split(',')]
 
-    # get user country (US, UK, DE)
     def _get_country(self):
+        """ Get user's country in two letter format (US, UK, DE, etc)
+
+        Returns:
+            str: Returns string of country
+        """
         return self.config.get('COUNTRY')
 
-    # return api key for *arr service
     def apikey(self, service):
+        """Gets the API key for the service from the config
+
+        Args:
+            service (str): Receives the string of sonarr or radarr
+
+        Returns:
+            str: Returns string of API key
+        """
         return self.config.get(f"{service.upper()}_APIKEY")
 
-    # return host:port of *arr service
     def host(self, service):
+        """Gets the host:port of the *arr service
+
+        Args:
+            service (str): Receives the string of sonarr or radarr
+
+        Returns:
+            str: Returns string of host:port for *arr service
+        """
         return self.config.get(f"{service.upper()}_HOST")
 
-# Parsing function to see if a flatrate provider is streaming
+
 def parse_streaming(streaming_services, user_services):
+    """Parse to find flatrate in streaming services and match on users subscriptions
+
+    Args:
+        streaming_services (dict): Dict of the media streaming options
+        user_services (list): List of user's streaming subscriptions
+
+    Returns:
+        list: Returns a list of all the streaming service matches for the users subscriptions
+    """
     streaming_on = []
 
     # parse streaming providers excluding rent and buy for services
@@ -121,9 +200,19 @@ def parse_streaming(streaming_services, user_services):
                 streaming_on.append(serv.get("provider_name"))
     return streaming_on
 
-# Sort through providers and media for streaming vs not while updating DB
 def compare_streaming(service, subscriptions, media_type):
+    """Sort through all the media and the streaming options matched with the users
+       streaming subscriptions and puts them into a list of IDs to set the monitor 
+       status in the *arr service.
 
+    Args:
+        service (str): String of sonarr or radarr
+        subscriptions (list): List of user's streaming subscriptions
+        media_type (str): String of movie or series
+
+    Returns:
+        tuple: Returns a tuple of lists, one of IDs for matched streams and one without
+    """
     # List for IDs for *arr service monitor status
     stop_monitor = []
     start_monitor = []
@@ -156,7 +245,7 @@ def main():
     # init the config to get api keys
     c = Config()
     tmdb = TMDB(c.apikey('tmdb'), c.country)
-    #sonarr = Arr(c.apikey('sonarr'), c.host('sonarr'), 'series')
+    sonarr = Arr(c.apikey('sonarr'), c.host('sonarr'), 'series')
     radarr = Arr(c.apikey('radarr'), c.host('radarr'), 'movie') 
 
     # parse stream providers vs user services to find if on streaming service
@@ -164,12 +253,18 @@ def main():
     stop_sonarr, start_sonarr = compare_streaming(sonarr, c.user_services, 'series')
 
     # set Movies in Radarr monitor status
-    radarr.stop_monitor(ids=stop_monitor)
-    radarr.start_monitor(ids=start_monitor)
+    radarr.stop_monitor(ids=stop_radarr)
+    radarr.start_monitor(ids=start_radarr)
 
     # set Series in Sonarr monitor status
-    sonarr.stop_monitor(ids=stop_monitor)
-    sonarr.start_monitor(ids=start_monitor)
+    sonarr.stop_monitor(ids=stop_sonarr)
+    sonarr.start_monitor(ids=start_sonarr)
+    
+    # remove media file from radarr 
+    radarr.remove_media(ids=stop_radarr)
+
+    # remove media file from sonarr 
+    sonarr.remove_media(ids=stop_sonarr)
 
     return
 
@@ -181,6 +276,8 @@ def get(url):
 def put(url, data):
     return requests.put(url=url, data=data)
 
+def delete(url):
+    return requests.delete(url=url)
 
 ### Database functions ###
 
